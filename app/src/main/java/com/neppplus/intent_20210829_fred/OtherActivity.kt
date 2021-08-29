acfaa979258a82c8bd01ae6eb8001d39a6ad5999
박 광 현 <elffire88@gmail.com>
2021-08-29 14:57:29 +0900

    다른화면에서 > 메인화면으로 이동 화면이 계속 쌓이는 문제 발생

--- app/src/main/java/com/neppplus/intent_20210829_fred/OtherActivity.kt
+++ app/src/main/java/com/neppplus/intent_20210829_fred/OtherActivity.kt
@@ -1,11 +1,22 @@
 package com.neppplus.intent_20210829_fred
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        otherActivityTxt.setOnClickListener {
+
+            val otherIntent = Intent(this, MainActivity::class.java)
+            startActivity(otherIntent)
+
+        }
+
+
     }
 }
